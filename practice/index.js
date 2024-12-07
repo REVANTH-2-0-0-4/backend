41eaee17666efa63bcd0ce584b281a0e756cb151
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2024-12-08 02:11:55 +0530

    done some practice

--- practice/index.js
+++ practice/index.js
@@ -1,12 +1,39 @@
+const fs = require('fs');
 const express = require('express');
-const path = require('path');
 const app = express();
-app.set('view engine','ejs');
+const path = require('path');
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-app.use(express.static(path.join(__dirname,'public')));
-app.get('/',(req,res)=>{
-    res.render("index");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    fs.readdir("./files", function (err, files) {
+        res.render("index", { files: files });
+    })
 })
-app.listen(3000,()=>{console.log("server running with port number 3000")});
+app.get('/show/:filename', (req, res) => {
+    fs.readFile(`./files${req.params.filename}`, (err, ds) => {
+        if (err) console.log(err);
+        else res.render("show", { filename: req.params.filename, data: ds });
+
+
+    })
+})
+
+app.post('/submit', (req, res) => {
+    fs.writeFile(`./files/${req.body.filename.split(' ').join('')}.txt`, req.body.description, function (err) {
+        if (err) {
+            console.error('Error writing file:', err);
+            // return res.status(500).send('Failed to write file');
+        } else {
+            console.log(' file  created successfully!');
+            res.redirect("/");
+        }
+    });
+
+
+
+})
+
+app.listen(3000, () => { console.log("server running with port number 3000") });
 
