645b4f47b7c7b7dbe4ddc8890cb17495b203f263
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2024-12-28 23:12:44 +0530

    upwards and onwards

--- chessclone/app.js
+++ chessclone/app.js
@@ -1,4 +1,28 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
 const app = express();
-app.get("/",indexRouter); 
+const server = http.createServer(app);
+const io = socket(server);
+const chess = new Chess();
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+let players = {};
+let currentplayer = "W"
+app.get("/", (req, res) => {
+    res.render("index", { title: "Custom Chess Game" });
+})
+io.on("connection", (socket) => {
+    console.log("connected");
+    
+socket.on("disconnect", () => {
+    console.log("disconnected");
+})
+
+})
+server.listen(3000, () => {
+    console.log("the server is running with a port number of 3000");
+});
